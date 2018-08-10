edd7fb42afef1b10d4c2978ecd2b34b94eecb0b6
linpengcheng <pengcheng.lin@quvideo.com>
2018-08-10 14:09:54 +0800

    update

--- Public/Test_data.py
+++ Public/Test_data.py
@@ -16,12 +16,12 @@ def generate_test_data(devices):
         dict_tmp[d['serial']]['user_name'] = ReadConfig().get_testdata('user_name')[devices.index(d)]
         dict_tmp[d['serial']]['password'] = ReadConfig().get_testdata('password')[devices.index(d)]
     with open(data_path, "w") as f:
-        json.dump(dict_tmp, f)
+        json.dump(dict_tmp, f, ensure_ascii=False)
         f.close()
     print("Test data data.json generated success")
 
 
 def get_test_data(d):
-    with open(data_path, 'r') as f:
+    with open(data_path, 'r', encoding='UTF-8') as f:
         data = json.load(f)
     return data[d.device_info['serial']]
